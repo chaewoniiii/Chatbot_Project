3265e2b023a8bfe82f077f093e27f457c6be57fe
Ureto <wjdwogud137@naver.com>
2022-05-31 16:59:03 +0900

    관리자 메인페이지 수정
    버튼 기능 추가 - 임시 테스트로 학습및 테스트 구축시 변경

--- Chatbot_Web/static/js/cb_admin.js
+++ Chatbot_Web/static/js/cb_admin.js
@@ -1,5 +1,71 @@
 $(function(){
     
 })
+
+function learning(){
+    var data_num = $('#sel_data').val()
+    if($('#n_data').css('display', 'none')){
+        $('#n_data').css('display', 'block')
+        $('.btn_res').css('display', 'block')
+        $('#n_data').text(data_num + "개의 데이터를 학습시키겠습니까?")
+    }
+}
+
+function btn_cancel(){
+    $('#n_data').css('display', 'none')
+    $('.btn_res').css('display', 'none')
+}
+
+function btn_ok(flag){
+    if(flag == 1){
+        var maskHeight = $(document).height();
+        var maskWidth = window.document.body.clientWidth;
     
+        var mask = "<div id='mask' style='position:absolute; z-index:9000; background-color:#000000; display:none; left:0; top:0;'></div>";
+        var loadingImg = "";
+    
+        loadingImg += "<div id='loadingImg' class='position-relative'>";
+        loadingImg += "<img src='/static/img/Skype-balls-loader-unscreen.gif' class='position-absolute top-0 start-50 translate-middle'/>";
+        loadingImg += "</div>";
+    
+        $('body')
+            .append(mask)
+            .append(loadingImg)
+        
+        $('#mask').css({
+            'width' : maskWidth,
+            'height' : maskHeight,
+            'opacity' : '0.1'
+        });
+    
+        $('#mask').show();
+    
+        $('#loadingImg').show();
+    
+        setTimeout('closeLoadingWithMask()', 3000)
+    }
+    else if(flag == 2){
+        $('#test_res').css('display', 'none')
+        $('#test_btn').css('display', 'none')
+    }
+    
+    
+}
+
+function learn_test(){
+    $('#test_res').css('display', 'block')
+    $('#test_btn').css('display', 'block')
+}
+
+function closeLoadingWithMask(){
+    $('#mask, #loadingImg').hide();
+    $('#mask, #loadingImg').empty();
+
+    alert('학습이 완료되었습니다')
+
+    if($('#n_data').css('display', 'block')){
+        $('#n_data').css('display', 'none')
+        $('.btn_res').css('display', 'none')
+    }
+}
 
